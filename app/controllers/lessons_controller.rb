341b2e7f0a8c940b0ec4915b3fccc83774a2d5b4
Yoshi Moriyama <eyoshimoriyama@gmail.com>
2017-11-11 21:24:35 +0000

    Added requirements for lessons page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
   def show
+  end
+
+  private
+
+  def require_authorized_for_current_course
+    if not current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to view lessons." 
+    end
   end
 
   helper_method :current_lesson
